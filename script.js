b3b4ca68e6a5b6bbdd3a553d678bb44b75cb0e07
Darren Arichandran <d.arichandran@gmail.com>
2023-01-26 20:34:00 +0000

    Update script.js with nested conditions neccesary to play one game of rock, paper, scissor

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // need to workout how to choose a random string in the array//
 function getComputerChoice() {
 
-    const options = ["rock", "paper", "scissors"];
+    const options = ["rock", "paper", "scissor"];
 
     return  options[Math.floor(Math.random() * options.length)];
 }
@@ -10,12 +10,47 @@ function getComputerChoice() {
 console.log(getComputerChoice())
 
 //below need to to write boolean to work out rock>scissors etc and return a string that declares winner//
-/*function playRound(playerSelection, computerSelection) {
-    if playerSelection>computerSelection
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock"){
+        if (computerSelection === "scissor") {
+            return "win";
+        }
+        else if (computerSelection === "paper") {
+            return "lose";
+        }
+        else if (computerSelection === "rock") {
+            return "draw";
+        }
+    }
+
+    if (playerSelection === "paper") {
+        if (computerSelection === "scissor") {
+            return "lose";
+        }
+        else if (computerSelection === "paper") {
+            return "draw";
+        }
+        else if (computerSelection === "rock") {
+            return "win";
+        }
+    }
+
+    if (playerSelection === "scissor"){
+        if (computerSelection === "scissor") {
+            return "draw";
+        }
+        else if (computerSelection === "paper") {
+            return "win";
+        }
+        else if (computerSelection === "rock") {
+            return "lose";
+        }
+    }
   }
    
   const playerSelection = "rock";
   const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));*/
+  console.log(playRound(playerSelection, computerSelection));
   
 
