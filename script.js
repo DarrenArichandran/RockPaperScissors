3c4d4300726525af45cd50ef1983ef1617af5067
Darren Arichandran <d.arichandran@gmail.com>
2023-03-05 13:37:30 +0000

    Load working buttons and results

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 
-// need to workout how to choose a random string in the array//
+ 
 function getComputerChoice() {
 
     const options = ["rock", "paper", "scissor"];
 
     return  (options[Math.floor(Math.random() * options.length)]);
 }
-
-
-//below need to to write boolean to work out rock>scissors etc and return a string that declares winner [done!]//
 
 function playRound(playerSelection, computerSelection) {
    if (playerSelection === "rock") {
@@ -38,51 +35,24 @@ function playRound(playerSelection, computerSelection) {
             return "tie"  
     } else if (playerSelection != "rock" || "paper" || "scissor") {
         return " incorrect input, try again"
-    }     
-    }
-
-
-    
-  
-  /*
-  console.log(playRound(playerSelection, computerSelection));
-  console.log(computerSelection);
-  console.log(computerSelection + (" ,") + playerSelection + (" ,") + playerSelection1);
-*/
-
-function game() {
-      for  (let round = 1; round <= 5; round++){
-       
-    const playerSelection1 = prompt("Make your choice; rock,paper or scissor?");
-    let playerSelection = (playerSelection1).toLowerCase();
-    let computerSelection = getComputerChoice();
-
-       playRound(playerSelection, computerSelection)
-     console.log("Player" + " " + playRound(playerSelection, computerSelection));
-
-// score above is being stored once increased - need to look at possibly using an array to call value once score icreases? 
-//[done!] answer was way more simple, have score update outside of function //
-
-if (playRound(playerSelection, computerSelection) === "win") {
-    playerScore++
-} else if (playRound(playerSelection, computerSelection) === "lose" ) {
-    computerScore++
-}
-console.log( playerScore + " : " + computerScore)
-
-    }
-    if (playerScore > computerScore) {
-        console.log("player is the winner");
-    } else if (playerScore < computerScore) {
-        console.log("computer is the winner"); 
-    } else {
-        console.log("its a tie")
-    }
+    }    
 }
 
-let playerScore = 0;
-let computerScore = 0; 
-console.log(game())
+const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.id;
+        let result = playRound(playerSelection, getComputerChoice());
+        
+        const scores = document.querySelector('#scores');
+
+        const content = document.createElement('div');
+        content.classList.add('content');
+        content.textContent = result;
+        
+        scores.appendChild(content);
+    });
+  });
 
 
 
@@ -91,4 +61,5 @@ console.log(game())
 
 
 
-  
+
+
